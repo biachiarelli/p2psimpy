04c20945e965a2333694a01d80d0222bb846949e
rezendehugo <hugorezende96@hotmail.com>
2020-06-03 00:21:26 -0300

    planning metrics test

--- tests/common/test_planning_metrics.py
+++ tests/common/test_planning_metrics.py
@@ -26,12 +26,12 @@ task3 = Task("task3")
 
 task2.addApplicableContext(c2)
 
-task1.setProvidedQuality(None, MpersMetrics.SECONDS, 20)
-task1.setProvidedQuality(None, MpersMetrics.ERROR, 80)
-task2.setProvidedQuality(None, MpersMetrics.SECONDS, 60)
+task1.setProvidedQuality(None, MpersMetrics.SECONDS, 30)
+task1.setProvidedQuality(None, MpersMetrics.ERROR, 30)
+task2.setProvidedQuality(None, MpersMetrics.SECONDS, 70)
 task2.setProvidedQuality(None, MpersMetrics.ERROR, 10)
-task3.setProvidedQuality(None, MpersMetrics.SECONDS, 100)
-task3.setProvidedQuality(None, MpersMetrics.ERROR, 20)
+task3.setProvidedQuality(None, MpersMetrics.SECONDS, 10)
+task3.setProvidedQuality(None, MpersMetrics.ERROR, 70)
 
 goal = Pragmatic(Decomposition.OR, "g1")
 
@@ -39,18 +39,40 @@ goal.addDependency(task1)
 goal.addDependency(task2)
 goal.addDependency(task3)
 
-qc1 = QualityConstraint(None, MpersMetrics.SECONDS, 180, Comparison.LESS_THAN)
+qc1 = QualityConstraint(None, MpersMetrics.SECONDS, 100, Comparison.LESS_THAN)
 qc1 = QualityConstraint(None, MpersMetrics.ERROR, 90, Comparison.LESS_THAN)
-qc2 = QualityConstraint(c2, MpersMetrics.SECONDS, 90, Comparison.LESS_THAN)
+qc2 = QualityConstraint(c2, MpersMetrics.SECONDS, 40, Comparison.LESS_THAN)
+qc3 = QualityConstraint(c4, MpersMetrics.ERROR, 40, Comparison.LESS_THAN)
+
 goal.interp.addQualityConstraint(qc1)
 goal.interp.addQualityConstraint(qc2)
+goal.interp.addQualityConstraint(qc3)
 
-
-def test_C1():
-    fullContext = [c1, c2, c3, c4]
+def test_error():
+    fullContext = [c1, c2, c4]
 
     plan = Planning().isAchievable(goal, fullContext, goal.interp)
 
     assert plan is not None
 
-    assert True is assertPlan(plan, [task2])
+    assert True is assertPlan(plan, [task1])
+
+
+def test_faster():
+    fullContext = [c1, c3, c2]
+
+    plan = Planning().isAchievable(goal, fullContext, goal.interp)
+
+    assert plan is not None
+
+    assert True is assertPlan(plan, [task1])
+
+
+def test_better():
+    fullContext = [c1, c3, c4, c2]
+
+    plan = Planning().isAchievable(goal, fullContext, goal.interp)
+
+    assert plan is not None
+
+    assert True is assertPlan(plan, [task1])
